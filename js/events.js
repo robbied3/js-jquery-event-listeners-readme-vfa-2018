736bf5977e7bec83674bd4c01e586fd056737acb
Robbie Dornbush <dornbush@gwu.edu>
2018-06-27 21:36:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
 
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+  return;
+}
+ 
+
+function frameIt() {
+  $("img").addClass("tasty")
+  return;
+}
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if(key.which == 71){
+    alert('You have pressed the "g" key!');
+    }
+  });
+  return;
+}
+ 
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+  return;
+}
+ 
 $(document).ready(function(){
-
-// call functions here
-
-});
+getIt()
+frameIt()
+pressIt()
+submitIt()
+ });
