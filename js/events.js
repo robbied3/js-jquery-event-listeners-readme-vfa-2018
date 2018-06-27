719ad2b68c6c9a05cc5c35f1dabffc44bf42a9c9
Robbie Dornbush <dornbush@gwu.edu>
2018-06-27 21:11:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,3 +1,5 @@
 $('p').on("click", function(getIt){"Hey!"});
 $('img').on('load',function(frameIt){"tasty"}); 
+$(document).on('keydown',function(key){if(key.which == G){alert('g was pressed');}
+});
 $("form").on("sumbit", function(submitIt){"Your form is going to be submitted now"}); 
