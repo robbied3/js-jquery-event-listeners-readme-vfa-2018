e945ff608fcc8f7172833a3a0d196527e19a266f
Robbie Dornbush <dornbush@gwu.edu>
2018-06-27 21:36:11 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,36 @@
-function getIt {$('p').on("click", function(){"Hey!"});
-$('img').on('load',function(){"tasty"}); 
-$(document).on('keydown',function(key){if(key.which == G){alert('g was pressed');
+
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+  return;
 }
-});
-$("form").on("sumbit", function(){"Your form is going to be submitted now"}); 
+ 
+
+function frameIt() {
+  $("img").addClass("tasty")
+  return;
+}
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if(key.which == 71){
+    alert('You have pressed the "g" key!');
+    }
+  });
+  return;
+}
+ 
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+  return;
+}
+ 
+$(document).ready(function(){
+getIt()
+frameIt()
+pressIt()
+submitIt()
+ });
